f38491cb206a38d9cb347fcdf71e26d1b05643eb
toni <ferreirocouto@gmail.com>
2020-10-03 18:26:30 +0100

    añadida funcionalidad button setOnclicklistener

--- app/src/main/java/com/toni/peeperAndroid/MainActivity.java
+++ app/src/main/java/com/toni/peeperAndroid/MainActivity.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void miMetodo(View view){
-        Toast.makeText(this, "Pulsaste el botón!", Toast.LENGTH_SHORT).show();
+        btn = findViewById(R.id.buttonMain);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Pulsaste el botón!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
